3093a2ccdc9c903047b3dd53041f2d99799145cd
saeed-jlnsi <saeed.jalinoosi@gmail.com>
2018-10-17 22:54:02 +0330

    Header component  & banner-image & reset.scss

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <div :class="$style.Banner">
+      <img src="../../assets/image/banner.jpg" alt="Background_Header">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" module>
+  @import './header';
+
+  /* h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  } */
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <app-header></app-header>
+        
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header/Header';
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+
+        }    
+    },
+    components: {
+        appHeader : Header,
+    }
+}
+</script>
+
+<style lang="scss" module>
+    @import './home';
+</style>
